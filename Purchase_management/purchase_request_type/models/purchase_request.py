65b4ba06f7609780815aebe92afc11385f43eb8b
ATCHE Maurice <maurice.atche@halltech-afria.com>
2023-01-10 18:27:31 +0000

    Fix warning message on date_required in purchase_request_type

--- Purchase_management/purchase_request_type/models/purchase_request.py
+++ Purchase_management/purchase_request_type/models/purchase_request.py
@@ -20,8 +20,8 @@ class PurchaseRequest(models.Model):
     picking_type_id = fields.Many2one(required=False)
     is_for_project = fields.Boolean(string="Imputer au projet", default=True)
     requested_by = fields.Many2one('res.users', string="Demandeur DA", readonly=True)
-    date_required = fields.Date(string="Request Date", tracking=True, default=lambda self:self._default_date_required())
-    date_approve = fields.Date(string="Date Approve")
+    date_required = fields.Date(string="Request Date", default=lambda self:self._default_date_required())
+    date_approve = fields.Date(string="Date Approve", tracking=True)
     
     
     def _compute_is_project_approver(self):
