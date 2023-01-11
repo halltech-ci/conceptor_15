dfaafac7d88be993fd234c31a7231e6bd80fb89b
ATCHE Maurice <maurice.atche@halltech-africa.com>
2023-01-11 13:08:31 +0000

    Adding purchase_report

--- Purchase_management/purchase_report/__manifest__.py
+++ Purchase_management/purchase_report/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'report/purchase_order_custom_report.xml',
+        #'report/purchase_order_custom_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
