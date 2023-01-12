333a7e91e79efd175fef7a6e8f2501bc5c4ba722
nevrit ahua <nevritahua.na@gmail.com>
2023-01-12 01:33:12 +0000

    Mise en place du bon de commande de conceptor à finaliser

--- hta_document_sign/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ hta_document_sign/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "purchase_request_custom",
+    'name': "hta_document_sign",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,25 +16,20 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Purchase Management',
-    'version': '15.0.1',
+    'category': 'Uncategorized',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['purchase_request'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/purchase_request_views.xml',
-        'report/purchase_order_custom_report.xml',
-        'report/purchase_request_custom_report.xml',
-        'report/purchase_request_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
-    'license': 'LGPL-3',
 }
