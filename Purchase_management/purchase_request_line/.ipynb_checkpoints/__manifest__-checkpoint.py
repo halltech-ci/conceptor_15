6b8f496af5e34403b2dc56a99f737686211e2ed5
ATCHE Maurice <maurice.atche@halltech-africa.com>
2023-01-21 04:47:54 +0000

    Adding custom purchase_request_line in purchase_request

--- Purchase_management/purchase_request_line/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ Purchase_management/purchase_request_line/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "purchase_request_analytic",
+    'name': "purchase_request_line",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,18 +16,18 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Purchase/Management',
+    'category': 'Purchase Management',
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['purchase_request', 'purchase_request_sale', 'purchase_request_type'],
+    'depends': ['purchase_request_custom', 'purchase_request_sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/purchase_request_analytic_views.xml',
+        'views/purchase_request.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
