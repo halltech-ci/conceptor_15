a0cb44f5338a7b217018252fad95c67b7cbc7f55
ATCHE Maurice <maurice.atche@halltech-africa.com>
2023-01-23 01:43:10 +0000

    Updating custom_sale_report

--- Purchase_management/purchase_request_type/models/purchase_request.py
+++ Purchase_management/purchase_request_type/models/purchase_request.py
@@ -6,7 +6,7 @@ from datetime import date, datetime, timedelta
 
 class PurchaseRequest(models.Model):
     _inherit= 'purchase.request'
-    _inherit = ["mail.thread", "mail.activity.mixin"]
+    #_inherit = ["mail.thread", "mail.activity.mixin"]
     
     
     @api.model
@@ -21,7 +21,7 @@ class PurchaseRequest(models.Model):
     picking_type_id = fields.Many2one('stock.picking.type', required=False)
     is_for_project = fields.Boolean(string="Imputer au projet", default=True)
     requested_by = fields.Many2one('res.users', string="Demandeur DA", readonly=True)
-    date_required = fields.Date(string="Request Date", default=lambda self:self._default_date_required(), tracing=True)
+    date_required = fields.Date(string="Request Date", default=lambda self:self._default_date_required(), tracking=True)
     date_approve = fields.Date(string="Date Approve", tracking=True)
     
     
